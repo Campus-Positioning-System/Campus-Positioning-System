c5e7ac5b04cfc0433912a8a43d8afda601973f20
hausertt <92016751+hausertt@users.noreply.github.com>
2022-06-14 13:03:04 +0200

    removed import

--- app/src/main/java/com/example/campus_positioning_system/Database/AppDatabase.java
+++ app/src/main/java/com/example/campus_positioning_system/Database/AppDatabase.java
@@ -1,8 +1,10 @@
-package com.example.campus_positioning_system;
+package com.example.campus_positioning_system.Database;
 
 import androidx.room.Database;
 import androidx.room.RoomDatabase;
 import androidx.room.TypeConverters;
+
+import com.example.campus_positioning_system.NNObject;
 
 
 @Database(entities = {NNObject.class}, version = 1)
