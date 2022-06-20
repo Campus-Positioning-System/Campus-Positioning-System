643b0b494a0ec7bcacfb1770810e87d9696ed2d4
DetectiveDuck5 <21358774+DetectiveDuck5@users.noreply.github.com>
2022-06-20 14:01:52 +0200

    Minor roomlist Icon changes (bugged xD)

--- app/src/main/java/com/example/campus_positioning_system/RoomList/RoomListViewHolder.java
+++ app/src/main/java/com/example/campus_positioning_system/RoomList/RoomListViewHolder.java
@@ -1,9 +1,11 @@
 package com.example.campus_positioning_system.RoomList;
 
 
+import android.graphics.drawable.Icon;
 import android.icu.util.ValueIterator;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 
@@ -23,6 +25,7 @@ import java.io.File;
 public class RoomListViewHolder extends TreeViewHolder {
 
     private TextView text;
+    private ImageView icon, start_button;
 
     //ToDo Fill other fields with text
     //ToDo add eventListener for navigation start
@@ -31,6 +34,8 @@ public class RoomListViewHolder extends TreeViewHolder {
     public RoomListViewHolder(@NonNull View itemView) {
         super(itemView);
         text = itemView.findViewById(R.id.room_item_desc);
+        icon = itemView.findViewById(R.id.list_icon);
+        start_button = itemView.findViewById(R.id.item_start_icon);
     }
 
     @Override
@@ -39,8 +44,11 @@ public class RoomListViewHolder extends TreeViewHolder {
         System.out.println("Binding value with class " + node.getValue().getClass().toString());
         if (node.getValue() instanceof Element){
             text.setText(((Element) (node.getValue())).getAttribute("roomname"));
+            icon.setImageResource(R.drawable.ic_baseline_sensor_door_24);
+            start_button.setVisibility(View.VISIBLE);
         }else {
             text.setText(node.getValue().toString());
+            start_button.setVisibility(View.INVISIBLE);
         }
 
     }
